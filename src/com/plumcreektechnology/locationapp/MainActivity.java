89a36678a4ae5f0ad542f4dbaf3269e0d618fd0a
Devin Frenze <frenze.devin@gmail.com>
2013-06-25 14:06:02 -0400

    beginning of LocationApp

--- src/com/plumcreektechnology/locationapp/MainActivity.java
+++ src/com/plumcreektechnology/locationapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.plumcreektechnology.locationapp;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+import android.app.Dialog;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,4 +24,15 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	private boolean servicesConnected() {
+		// check for Google Play
+		int result = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		if(ConnectionResult.SUCCESS == result) {
+			Log.d("Location Updates", "Google Play services is available.");
+			return true;
+		} else {
+			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(result, this, 0);
+			
+		}
+	}
 }

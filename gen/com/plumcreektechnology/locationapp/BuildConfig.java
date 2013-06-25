1c71d6f2170f1f99e94efd20ccdc13189f505de8
Devin Frenze <frenze.devin@gmail.com>
2013-06-25 13:37:52 -0400

    blank location project

---
+++ gen/com/plumcreektechnology/locationapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.plumcreektechnology.locationapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
